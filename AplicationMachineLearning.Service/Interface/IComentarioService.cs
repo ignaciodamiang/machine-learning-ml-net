9383c97f43ed36ae5dcc6dbe920e2faac6a0f015
LeandroBu <leandro.ariel.gomez1@gmail.com>
2022-06-12 23:07:53 -0300

    Se actualiza capas de servicio y repositorio y ademas se agrega a la vista las ultimas 10 cristicas de peliculas

---
+++ AplicationMachineLearning.Service/Interface/IComentarioService.cs
@@ -0,0 +1,15 @@
+﻿using EF.Data.EF;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AplicationMachineLearning.Service.Interface
+{
+    public interface IComentarioService
+    {
+        string EvaluarComentarios(string comentario);
+        List<EvaluarComentario> ObtenerListaDeComentarios();
+    }
+}
---
+++ EF.Data/Repository/Interface/IComentarioRepository.cs
@@ -0,0 +1,17 @@
+﻿using EF.Data.EF;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EF.Data.Repository.Interface
+{
+    public interface IComentarioRepository
+    {
+        void GuardarComentario(EvaluarComentario evaluarComentario);
+        void Savechange();
+        List<EvaluarComentario> ObtenerListaDeComentarios();
+        void EliminarSobranteDeLista(EvaluarComentario evaluarComentario);
+    }
+}
